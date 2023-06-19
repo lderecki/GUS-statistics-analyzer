0fb3caf8cf156b12c910054f15272a4cb3af5a24
Lukasz <lukasz.derecki@wp.pl>
2023-06-19 23:50:54 +0200

    init commit of new microservice gus-api-consumer

---
+++ gus-api-consumer/src/main/java/pl/lderecki/gusapiconsumer/GusApiConsumerApplication.java
@@ -0,0 +1,13 @@
+package pl.lderecki.gusapiconsumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GusApiConsumerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GusApiConsumerApplication.class, args);
+	}
+
+}
---
+++ gus-api-consumer/src/test/java/pl/lderecki/gusapiconsumer/GusApiConsumerApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.lderecki.gusapiconsumer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GusApiConsumerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
